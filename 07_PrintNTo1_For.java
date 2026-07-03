b202bb2568aecc3e63cc887dbee817d3685fc20c
srishivacharan <srishivacharan011@gmail.com>
2026-07-03 16:36:55 +0530

    Create 07_PrintNTo1_For.java

---
+++ 07_PrintNTo1_For.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class PrintNTo1_For {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
